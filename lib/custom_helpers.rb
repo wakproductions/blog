1dfa4d62c8c0425e715f4f049775b4c1f145a59d
Winston Kotzan <wak@wakproductions.com>
2019-03-21 22:55:19 -0400

    Make og:url full

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -2,4 +2,8 @@ module CustomHelpers
   def social_media_image(meta_image_path)
     URI.join(config[:base_url], meta_image_path || 'images/blog-default-social-media-image.jpg')
   end
+
+  def social_media_url(meta_url)
+    URI.join(config[:base_url], meta_url)
+  end
 end
